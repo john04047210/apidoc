15fdc180bf930125e26d48df62e52e6105842a47
Peng <john_04047210@163.com>
2019-12-23 14:00:31 +0900

    add locale file ja.js

---
+++ template/locales/ja.js
@@ -0,0 +1,25 @@
+define({
+    ja: {
+        'Allowed values:'             : '有効値:',
+        'Compare all with predecessor': '以前すべてのバージョンと比較',
+        'compare changes to:'         : '指定バージョンと比較:',
+        'compared to'                 : '比較',
+        'Default value:'              : 'デフォールト値:',
+        'Description'                 : '説明',
+        'Field'                       : 'フィールド',
+        'General'                     : '概要',
+        'Generated with'              : '基づき',
+        'Name'                        : '名称',
+        'No response values.'         : '返り値なし.',
+        'optional'                    : 'オプション',
+        'Parameter'                   : 'パラメータ',
+        'Permission:'                 : '権限:',
+        'Response'                    : 'レスポンス',
+        'Send'                        : '発送',
+        'Send a Sample Request'       : '発送請求例',
+        'show up to version:'         : '指定バージョンに表示:',
+        'Size range:'                 : '値の範囲:',
+        'Type'                        : 'タイプ',
+        'url'                         : 'URL'
+    }
+});
